cb609d08bd8e36011e19f45d630efec1ffec9cc5
Fredrik Junede <fredrik.junede@gmail.com>
2019-08-25 17:30:16 +0200

    Updated premake for new folder structure

--- premake5.lua
+++ premake5.lua
@@ -8,11 +8,11 @@ project "ResourceManager"
 
 	files {
 		"ResourceManager/src/Defines.h",
-		"ResourceManager/src/ResManAPI/ResourceManager.cpp",
-		"ResourceManager/src/ResManAPI/ResourceManager.h",
-		"ResourceManager/src/ResManAPI/Resources/Resource.h",
-		"ResourceManager/src/ResManAPI/FormatLoaders/FormatLoader.cpp",
-		"ResourceManager/src/ResManAPI/FormatLoaders/FormatLoader.h",
+		"ResourceManager/src/ResourceManager.cpp",
+		"ResourceManager/src/ResourceManager.h",
+		"ResourceManager/src/Resources/Resource.h",
+		"ResourceManager/src/FormatLoaders/FormatLoader.cpp",
+		"ResourceManager/src/FormatLoaders/FormatLoader.h",
 		"ResourceManager/src/miniz/miniz.h",
 		"ResourceManager/src/miniz/miniz.c"
 	}
